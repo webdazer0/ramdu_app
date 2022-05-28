b33285bb643f03145106901e767188387b7fe81a
Miguel C <webdazer0@gmail.com>
2022-05-28 02:51:02 +0200

    change name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ramdu_app/main.dart';
+import 'package:tres_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
